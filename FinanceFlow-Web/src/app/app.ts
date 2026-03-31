be4a628f538a0c09b7b5ffa09f425852ad6edc0d
Juliano Galhardo de Oliveira <julianogalhardo@192.168.0.12>
2026-03-31 18:18:35 -0300

    fix: resolve zone.js dependency and prepare auth feature structure

--- FinanceFlow-Web/src/app/app.ts
+++ FinanceFlow-Web/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router'; // 1. Importar o símbolo
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet], // 2. Adicionar aqui para o HTML reconhecer
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('FinanceFlow-Web');
+export class AppComponent {
+  title = 'FinanceFlow-Web';
 }
